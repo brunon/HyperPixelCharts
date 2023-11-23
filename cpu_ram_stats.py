0164fc9f65041733a323de6999d8aa0664172277
Bruno Navert <bruno.navert@gmail.com>
2023-11-23 09:56:23 -0500

    Add sd_card_writes.sh call in Python script

--- cpu_ram_stats.py
+++ cpu_ram_stats.py
@@ -12,6 +12,9 @@ import psutil
 import shutil
 import argparse
 from datetime import datetime
+import subprocess
+
+script_dir = os.path.dirname(__file__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--csv', required=True, help="CSV output dir")
@@ -21,6 +24,7 @@ args = parser.parse_args()
 cpu_usage = round(psutil.cpu_percent(args.polling), 1)
 ram_usage = round(psutil.virtual_memory()[2], 1)
 disk_usage = round(psutil.disk_usage('/').percent, 1)
+sd_card_writes = int(subprocess.check_output(["/bin/sh", os.path.join(script_dir, "sd_card_writes.sh")], text=True).strip())
 hostname = socket.gethostname()
 ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 csv_dir = args.csv
@@ -28,5 +32,5 @@ csv_file = os.path.join(csv_dir, hostname + '.csv')
 
 with open(csv_file, 'a') as f:
     writer = csv.writer(f)
-    writer.writerow((hostname, cpu_usage, ram_usage, disk_usage, ts))
+    writer.writerow((hostname, cpu_usage, ram_usage, disk_usage, ts, sd_card_writes))
 

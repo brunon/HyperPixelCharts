6595c5139a30410719ad224f181aa8e6d9babd2a
Bruno Navert <bruno.navert@gmail.com>
2024-07-30 18:09:14 -0400

    Proper total height including offset

--- combine_images.py
+++ combine_images.py
@@ -8,28 +8,28 @@ from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--images', dest='images', required=True, help="Glob of images to combine")
+parser.add_argument('--offset', dest='offset', required=False, type=int, default=50, help="Vertical offset (spacing)")
 args = parser.parse_args()
 
-def combine_images(output_path):
+def combine_images(output_path, offset_height):
     image_paths = glob.glob(args.images)
     chosen_images = random.choices(image_paths, k=2)
     images = [Image.open(x) for x in chosen_images]
     widths, heights = zip(*(i.size for i in images))
     max_width = max(widths)
-    total_height = sum(heights)
+    total_height = sum(heights) + offset_height
     new_im = Image.new('RGB', (max_width, total_height), 'WHITE')
     y_offset = 0
     for im in images:
         new_im.paste(im, (0, y_offset))
-        y_offset += im.size[1] + 50
+        y_offset += im.size[1] + offset_height
     new_im.save(output_path)
     return chosen_images
 
 if __name__ == '__main__':
     temp_image = "/tmp/combined_image_temp.png"  # save to tmpfs dir
     final_image = "/tmp/combined_image.png"
-    chosen_images = combine_images(temp_image)
+    chosen_images = combine_images(temp_image, args.offset)
     print(datetime.now(), "- Combining images:", ' & '.join(chosen_images))
     os.rename(temp_image, final_image)
-
 
